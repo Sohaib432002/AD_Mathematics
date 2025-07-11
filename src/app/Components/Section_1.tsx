46ec90ec7f49e01a3ffbaa8b1fb069e8016dec36
sohaib <sohaibmaqsoodsohaibmaqsood@gmail.com>
2025-07-11 05:50:39 +0500

    Added Home page and Navbar

--- src/app/Components/Section_1.tsx
+++ src/app/Components/Section_1.tsx
@@ -5,8 +5,8 @@ function Section_1() {
     const newLocal = "url('./frame/Ellipse 1 (3).png') no-repeated bottom ";
     return (
         <>
-            <section className='font-loto m-7 flex justify-between max-[1000px]:flex-wrap' >
-                <div className=' w-[50%] max-[1000px]:w-[100%] max-[1000px]:text-center flex justify-center flex-col' style={{ backgroundImage: "url('./Vector.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <section className='font-loto m-7 flex justify-between max-[500px]:mx-0 max-[1000px]:flex-wrap' >
+                <div className=' w-[50%] max-[1000px]:w-[100%] max-[1000px]:text-center  flex justify-center flex-col' style={{ backgroundImage: "url('./Vector.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
                     <p className='text-[24px] text-[#707070] max-[550px]:text-[18px] m-0 font-semibold'>Hi I am </p>
                     <p className='text-[28px] text-[#959595] max-[550px]:text-[22px] m-0 font-bold' >Mahmood Fazile</p>
                     <h2 className="text-[70px] font-black bg-[linear-gradient(90deg,_#984300_0%,_#FD6F00_46%,_#CA5900_100%)] bg-clip-text text-transparent max-[550px]:text-[50px] ">
@@ -20,7 +20,7 @@ function Section_1() {
                             <div><img src="./frame/Becon.png" alt="png" /></div>
                         </div>
                         <div className="btn-section0">
-                            <button className="bg-[linear-gradient(90deg,_#FD6F00_0%,_#E46400_100%)] mr-10 text-[20px] text-white px-10 py-3 rounded cursor-pointer max-[550px]:text-[15px] max-[400px]:m-23">
+                            <button className="bg-[linear-gradient(90deg,_#FD6F00_0%,_#E46400_100%)] mr-10 text-[20px] text-white px-10 py-3 rounded cursor-pointer max-[550px]:text-[15px] max-[400px]:m-3">
                                 Hire Me
                             </button>
 
@@ -28,7 +28,7 @@ function Section_1() {
                         </div>
                     </div>
                     <div className='flex justify-start max-[1000px]:justify-center py-4'>
-                    <div className='flex my-10 items-center bg-[#1e2125] max-[850px]:w-[100%] max-[850px]:justify-center rounded p-4'>
+                    <div className='flex my-10 items-center bg-[#1e2125] max-[550px]:m-3 max-[850px]:w-[100%] max-[850px]:justify-center rounded p-4'>
 
                         <div className='flex justify-between flex-col max-[550px]:text-[17px] max-[550px]:text-left border-[#575757] font-extrabold border-r text-[20px]'>
                             <h3 className='py-3 text-[#FD6F00]'>5+</h3>
